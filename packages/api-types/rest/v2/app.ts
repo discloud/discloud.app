b4b0816053585c15235252af2eb8008069adbe4a
Gorniaky <hwss.s2@gmail.com>
2022-08-23 20:04:35 -0300

    more types for `app` route

--- packages/api-types/rest/v2/app.ts
+++ packages/api-types/rest/v2/app.ts
@@ -1,13 +1,5 @@
 import { RESTApiBaseResult } from "./base";
 
-export interface RESTGetApiApp extends RESTApiBaseResult {
-  apps: ApiApp
-}
-
-export interface RESTGetApiApps extends RESTApiBaseResult {
-  apps: ApiApp[]
-}
-
 export interface ApiApp {
   id: string,
   online: boolean,
@@ -19,3 +11,92 @@ export interface ApiApp {
   autoDeployGit: string,
   autoRestart: boolean
 }
+
+export interface ApiAppBackup {
+  id: string
+  status: string
+  url: string
+}
+
+export interface ApiAppLog {
+  id: string
+  terminal: ApiTerminal
+}
+
+export interface ApiTerminal {
+  big: string
+  small: string
+}
+
+export interface ApiAppCRUD {
+  alreadyInProcess: string[]
+  alreadyOffline: string[]
+  restarted: string[]
+}
+
+export interface ApiAppStatus {
+  id: string
+  container: string
+  cpu: string
+  last_restart: string
+  memory: string
+  netIO: ApiNetIO
+  ssd: string
+}
+
+export interface ApiNetIO {
+  down: string
+  up: string
+}
+
+export interface RESTGetApiAppAllResult extends RESTApiBaseResult {
+  apps: ApiApp[]
+}
+
+export interface RESTGetApiAppAllBackupResult extends RESTApiBaseResult {
+  backups: ApiAppBackup[]
+}
+
+export interface RESTGetApiAppAllLogResult extends RESTApiBaseResult {
+  apps: ApiAppLog[]
+}
+
+export interface RESTGetApiAppAllStatusResult extends RESTApiBaseResult {
+  apps: ApiAppStatus[]
+}
+
+export interface RESTGetApiAppResult extends RESTApiBaseResult {
+  apps: ApiApp
+}
+
+export interface RESTGetApiAppBackupResult extends RESTApiBaseResult {
+  backups: ApiAppBackup
+}
+
+export interface RESTGetApiAppLogResult extends RESTApiBaseResult {
+  apps: ApiAppLog
+}
+
+export interface RESTGetApiAppStatusResult extends RESTApiBaseResult {
+  apps: ApiAppStatus
+}
+
+export interface RESTApiAppCRUDResult extends RESTApiBaseResult {
+  apps: ApiAppCRUD
+}
+
+export type RESTDeleteApiAppAllDeleteResult = RESTApiAppCRUDResult
+
+export type RESTPutApiAppAllRestartResult = RESTApiAppCRUDResult
+
+export type RESTPutApiAppAllStartResult = RESTApiAppCRUDResult
+
+export type RESTPutApiAppAllStopResult = RESTApiAppCRUDResult
+
+export type RESTDeleteApiAppDeleteResult = RESTApiBaseResult
+
+export type RESTPutApiAppRestartResult = RESTApiBaseResult
+
+export type RESTPutApiAppStartResult = RESTApiBaseResult
+
+export type RESTPutApiAppStopResult = RESTApiBaseResult
