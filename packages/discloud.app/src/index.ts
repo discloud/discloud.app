cfca516631da13b13be24e052352c12590f72890
Gorniaky <hwss.s2@gmail.com>
2022-08-26 00:46:51 -0300

    export all files from lib

---
+++ packages/discloud.app/src/index.ts
@@ -0,0 +1,23 @@
+import DiscloudApp from "./discloudApp/DiscloudApp";
+import AppManager from "./managers/AppManager";
+import AppTeamManager from "./managers/AppTeamManager";
+import App from "./structures/App";
+import Team from "./structures/Team";
+import User from "./structures/User";
+import BitField from "./util/BitField";
+import ModPermissionsBF from "./util/ModPermissionsBF";
+
+export * from "@discloudapp/api-types/v2";
+export * from "@discloudapp/rest";
+export * from "./@types";
+export {
+  App,
+  AppManager,
+  AppTeamManager,
+  BitField,
+  DiscloudApp,
+  ModPermissionsBF,
+  Team,
+  User
+};
+
