2adf528e54793b302fd0e128f7ad5dbf2e5403af
Gorniaky <hwss.s2@gmail.com>
2022-08-28 10:41:33 -0300

    `arrayToMap` created in util

--- packages/discloud.app/src/util/index.ts
+++ packages/discloud.app/src/util/index.ts
@@ -4,6 +4,28 @@ import { readFileSync } from "node:fs";
 import { Stream } from "node:stream";
 import { request } from "undici";
 
+export function arrayToMap<T extends Record<any, any>>(array: T[], index: string): Map<string, T>
+export function arrayToMap<T extends Record<any, any>, H = unknown>(
+  array: T[],
+  index: string,
+  holds: { new(...args: any[]): H }
+): Map<string, H>
+export function arrayToMap<T extends Record<any, any>, H = unknown>(
+  array: T[],
+  index: string,
+  holds?: { new(...args: any[]): H }
+) {
+  const map = new Map<string, unknown>();
+
+  for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+
+    map.set(element[index], holds ? new holds(element) : element);
+  }
+
+  return map;
+}
+
 export async function resolveFile(file: string): Promise<RawFile> {
   if (typeof file === "string") {
     if (/^https?:\/\//.test(file))
