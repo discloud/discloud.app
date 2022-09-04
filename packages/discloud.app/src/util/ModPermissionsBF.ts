3e9d85e5ba5eac8917572fbe1dc9e254b01a1849
Gorniaky <hwss.s2@gmail.com>
2022-09-04 19:35:01 -0300

    All permissions added to ModPermissionsBF

--- packages/discloud.app/src/util/ModPermissionsBF.ts
+++ packages/discloud.app/src/util/ModPermissionsBF.ts
@@ -4,6 +4,7 @@ import { ModPermissions } from "./constants";
 
 export default class ModPermissionsBF extends BitField<ModPermissionsFlags, number> {
   static Flags = ModPermissions;
+  static All = new ModPermissionsBF(Object.values(ModPermissions)).toArray();
 }
 
 export type ModPermissionsResolvable = BitFieldResolvable<ModPermissionsFlags, number>
