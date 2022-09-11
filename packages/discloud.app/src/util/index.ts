98bc3cc9220d08666db577a352ae0ccfca72e59a
Gorniaky <hwss.s2@gmail.com>
2022-09-11 19:30:45 -0300

    fix: resolveFile file buffer type

--- packages/discloud.app/src/util/index.ts
+++ packages/discloud.app/src/util/index.ts
@@ -4,34 +4,12 @@ import { readFileSync } from "node:fs";
 import { Stream } from "node:stream";
 import { request } from "undici";
 
-export function arrayToMap<T extends Record<any, any>>(array: T[], index: string): Map<string, T>
-export function arrayToMap<T extends Record<any, any>, H = unknown>(
-  array: T[],
-  index: string,
-  holds: { new(...args: any[]): H }
-): Map<string, H>
-export function arrayToMap<T extends Record<any, any>, H = unknown>(
-  array: T[],
-  index: string,
-  holds?: { new(...args: any[]): H },
-) {
-  const map = new Map<string, unknown>();
-
-  for (let i = 0; i < array.length; i++) {
-    const element = array[i];
-
-    map.set(element[index], holds ? new holds(element) : element);
-  }
-
-  return map;
-}
-
 export async function resolveFile(file: string): Promise<RawFile> {
   if (typeof file === "string") {
     if (/^https?:\/\//.test(file))
       return {
         name: file.split("/").pop()!,
-        data: await request(file, { throwOnError: true }).then(res => res.body) as Buffer,
+        data: await request(file, { throwOnError: true }).then(res => res.body.blob()) as Buffer,
         key: "file",
       };
 
