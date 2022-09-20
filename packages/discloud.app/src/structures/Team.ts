61f7937af041c55e8b8888ca67abd814f38c8feb
Gorniaky <hwss.s2@gmail.com>
2022-09-19 21:06:48 -0300

    feat: TeamManager methods added to Team structure

--- packages/discloud.app/src/structures/Team.ts
+++ packages/discloud.app/src/structures/Team.ts
@@ -1,4 +1,5 @@
 import { ApiTeamApps } from "@discloudapp/api-types/v2";
+import { ModPermissionsFlags, UpdateAppOptions } from "../@types";
 import DiscloudApp from "../discloudApp/DiscloudApp";
 import Base from "./Base";
 
@@ -8,7 +9,7 @@ export default class Team extends Base {
   lang;
   name;
   online;
-  perms;
+  perms: ModPermissionsFlags[];
   ramKilled;
 
   constructor(discloudApp: DiscloudApp, data: ApiTeamApps) {
@@ -19,8 +20,52 @@ export default class Team extends Base {
     this.lang = data.lang;
     this.name = data.name;
     this.online = data.online;
-    this.perms = data.perms;
+    this.perms = data.perms as ModPermissionsFlags[];
     this.ramKilled = data.ramKilled;
+  }
+
+  backup() {
+    return this.discloudApp.team.backup(this.id);
+  }
+
+  setRam(quantity: number) {
+    return this.discloudApp.team.ram(this.id, quantity);
+  }
+
+  status() {
+    return this.discloudApp.team.status(this.id);
+  }
+
+  terminal() {
+    return this.discloudApp.team.terminal(this.id);
+  }
+
+  update(options: UpdateAppOptions) {
+    return this.discloudApp.team.update(this.id, options);
+  }
+
+  async restart() {
+    const data = await this.discloudApp.team.restart(this.id);
+
+    this.online = data.status === "ok";
+
+    return data;
+  }
+
+  async start() {
+    const data = await this.discloudApp.team.start(this.id);
+
+    this.online = data.status === "ok";
+
+    return data;
+  }
+
+  async stop() {
+    const data = await this.discloudApp.team.stop(this.id);
+
+    this.online = data.status === "ok";
+
+    return data;
   }
 
   toString() {
