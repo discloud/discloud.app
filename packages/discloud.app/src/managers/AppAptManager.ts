446338b7428e24009b59407a2c8a1d1be0a92f06
Gorniaky <hwss.s2@gmail.com>
2022-09-21 08:13:57 -0300

    feat: AppAptManager#app and AppTeamManager#app extends BaseApp

--- packages/discloud.app/src/managers/AppAptManager.ts
+++ packages/discloud.app/src/managers/AppAptManager.ts
@@ -1,11 +1,11 @@
 import { RESTDeleteApiAppAptResult, RESTPutApiAppAptResult, Routes } from "@discloudapp/api-types/v2";
 import { AptString } from "../@types";
 import DiscloudApp from "../discloudApp/DiscloudApp";
-import App from "../structures/App";
+import BaseApp from "../structures/BaseApp";
 import BaseManager from "./BaseManager";
 
-export default class AppAptManager extends BaseManager {
-  constructor(discloudApp: DiscloudApp, public app: App) {
+export default class AppAptManager<T extends BaseApp> extends BaseManager {
+  constructor(discloudApp: DiscloudApp, public app: T) {
     super(discloudApp);
   }
 
