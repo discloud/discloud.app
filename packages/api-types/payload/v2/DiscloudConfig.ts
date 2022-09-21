b73532ae1b95479d7feb337dd4978bb31981d6c0
Gorniaky <hwss.s2@gmail.com>
2022-09-21 20:40:45 -0300

    feat: Create discloud config types

---
+++ packages/api-types/payload/v2/DiscloudConfig.ts
@@ -0,0 +1,67 @@
+export type DiscloudConfig<T = AppTypes, V extends AppLanguages = AppLanguages> =
+  T extends "bot" ? DiscloudConfigBot<V> :
+  T extends "site" ? DiscloudConfigSite<V> :
+  BaseDiscloudConfig<V>
+
+interface BaseDiscloudConfig<V extends AppLanguages> {
+  APT: string
+  AUTORESTART: boolean
+  MAIN: string
+  RAM: number
+  TYPE: AppTypes
+  VERSION: Version<V>
+}
+
+interface DiscloudConfigBot<V extends AppLanguages> extends BaseDiscloudConfig<V> {
+  AVATAR: string
+  NAME: string
+  TYPE: "bot"
+}
+
+interface DiscloudConfigSite<V extends AppLanguages> extends BaseDiscloudConfig<V> {
+  ID: string
+  TYPE: "site"
+}
+
+export type AppTypes =
+  | "bot"
+  | "site"
+
+export type AppLanguages =
+  | "go"
+  | "java"
+  | "js"
+  | "php"
+  | "py"
+  | "rb"
+  | "rs"
+
+type Version<T extends AppLanguages = AppLanguages> =
+  T extends "go" ? VersionGo :
+  T extends "java" ? VersionJava :
+  T extends "js" ? VersionJs :
+  T extends "php" ? VersionPhp :
+  T extends "py" ? VersionPy :
+  T extends "rb" ? VersionRb :
+  T extends "rs" ? VersionRs :
+  BaseVersion
+
+type BaseVersion = "latest"
+
+type VersionGo = BaseVersion
+
+type VersionJava = BaseVersion
+
+type VersionJs = BaseVersion
+  | "current"
+  | "suja"
+
+type VersionPhp = BaseVersion
+
+type VersionPy = BaseVersion
+  | "suja"
+
+type VersionRb = BaseVersion
+
+type VersionRs = BaseVersion
+  | "suja"
--- packages/api-types/v2.ts
+++ packages/api-types/v2.ts
@@ -1,1 +1,2 @@
+export * from "./payload/v2";
 export * from "./rest/v2";
