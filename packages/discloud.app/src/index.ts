0a18873f34f283409681b4d60287e16322efe3a5
Gorniaky <hwss.s2@gmail.com>
2022-09-21 21:20:52 -0300

    chore: export all in main file

--- packages/discloud.app/src/index.ts
+++ packages/discloud.app/src/index.ts
@@ -1,7 +1,16 @@
 import DiscloudApp from "./discloudApp/DiscloudApp";
+import AppAptManager from "./managers/AppAptManager";
 import AppManager from "./managers/AppManager";
 import AppTeamManager from "./managers/AppTeamManager";
+import BaseManager from "./managers/BaseManager";
+import CachedManager from "./managers/CachedManager";
+import DataManager from "./managers/DataManager";
+import TeamManager from "./managers/TeamManager";
 import App from "./structures/App";
+import AppStatus from "./structures/AppStatus";
+import AppUploaded from "./structures/AppUploaded";
+import Base from "./structures/Base";
+import BaseApp from "./structures/BaseApp";
 import Team from "./structures/Team";
 import User from "./structures/User";
 import BitField from "./util/BitField";
@@ -13,12 +22,21 @@ export * from "./@types";
 export * from "./util";
 export {
   App,
+  AppAptManager,
   AppManager,
+  AppStatus,
   AppTeamManager,
+  AppUploaded,
+  Base,
+  BaseApp,
+  BaseManager,
   BitField,
+  CachedManager,
+  DataManager,
   DiscloudApp,
   ModPermissionsBF,
   Team,
+  TeamManager,
   User,
 };
 export const discloud = new DiscloudApp();
