99cb2b98a80a81c1cbae52c758cccd28c3a615ac
Gorniaky <hwss.s2@gmail.com>
2023-01-12 17:18:52 -0300

    Create `calculatePercentage` function

--- packages/util/src/index.ts
+++ packages/util/src/index.ts
@@ -2,3 +2,6 @@ export * from "./BitField";
 export * from "./FileResolver";
 export * from "./ModPermissionsBF";
 
+export function calculatePercentage(value: string | number, major: string | number) {
+  return Number((Number(value) / Number(major) * 100).toFixed(2));
+}
