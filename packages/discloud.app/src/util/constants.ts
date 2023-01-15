6a142d75d89ef643e99ca76b8375e9abd914d524
Gorniaky <hwss.s2@gmail.com>
2023-01-15 00:04:28 -0300

    chore: export library version

--- packages/discloud.app/src/util/constants.ts
+++ packages/discloud.app/src/util/constants.ts
@@ -1,5 +1,9 @@
+import { readFileSync } from "node:fs";
+import { join } from "node:path";
 import { env } from "node:process";
 import { DiscloudAppOptions } from "../@types";
+
+export const version: string = JSON.parse(readFileSync(join(__dirname, "package.json"), "utf8")).version;
 
 /**
  * Library default options
