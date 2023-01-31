ff35eb74797d21850cc8cb75255ea8f9a2aae71e
Gorniaky <hwss.s2@gmail.com>
2023-01-30 21:24:55 -0300

    feat: REST User-Agent enhancement

--- packages/rest/src/utils/contants.ts
+++ packages/rest/src/utils/contants.ts
@@ -1,5 +1,6 @@
 import { APIVersion } from "@discloudapp/api-types/v2";
 import { existsSync, readFileSync } from "node:fs";
+import { arch, platform, release, type } from "node:os";
 import { join } from "node:path";
 import { Agent } from "undici";
 import { RESTOptions } from "../@types";
@@ -13,9 +14,13 @@ export const DefaultRestOptions: Required<RESTOptions> = {
 } as const;
 
 const DiscloudAppPackageLocation = join(__dirname, "..", "..", "..", "..", "discloud.app", "package.json");
+export const os_name = type();
+export const os_release = release().split?.(".").slice(0, 2).join(".") ?? release();
+export const os_platform = platform();
+export const cpu_arch = arch();
 
 let discloudAppVersion = "";
 if (existsSync(DiscloudAppPackageLocation))
-  discloudAppVersion = ` v${JSON.parse(readFileSync(DiscloudAppPackageLocation, "utf8")).version}`;
+  discloudAppVersion = `/${JSON.parse(readFileSync(DiscloudAppPackageLocation, "utf8")).version}`;
 
-export const DefaultUserAgent = `DiscloudApp${discloudAppVersion}`;
+export const DefaultUserAgent = `DiscloudApp${discloudAppVersion} (${os_name} ${os_release}; ${os_platform}; ${cpu_arch})`;
