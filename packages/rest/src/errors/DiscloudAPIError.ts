88cfab3ed5c2e92677a2c796d3aa7a454f6c30d4
Gorniaky <hwss.s2@gmail.com>
2023-08-16 12:28:53 -0300

    rest: Create DiscloudAPIError structure

---
+++ packages/rest/src/errors/DiscloudAPIError.ts
@@ -0,0 +1,11 @@
+export class DiscloudAPIError extends Error {
+  constructor(
+    message: string,
+    public code: number | string,
+    public method: string,
+    public path: string,
+    public body: any,
+  ) {
+    super(`\x1b[31m[DISCLOUD API] ${message}\x1b[0m`);
+  }
+}
