68fc0f0d605a882a439db831e816f7d8103cce2d
Gorniaky <hwss.s2@gmail.com>
2025-03-17 13:43:29 -0300

    fix json import util

--- packages/util/src/JSON.ts
+++ packages/util/src/JSON.ts
@@ -1,8 +1,12 @@
 import { readFileSync } from "fs";
+import { readFile } from "fs/promises";
 import { normalize } from "path";
 
 const JSONs: Record<string, unknown> = {};
-export function importJSON<T>(path: string): Promise<T> {
+export function importJSON<T>(path: string): T
+export function importJSON<T>(path: string, async: true): Promise<T>
+export function importJSON(path: string, async?: boolean) {
   path = normalize(path);
+  if (async) return JSONs[path] ?? readFile(path, "utf8").then(content => JSONs[path] ??= JSON.parse(content));
   return JSONs[path] ??= JSON.parse(readFileSync(path, "utf8"));
 }
