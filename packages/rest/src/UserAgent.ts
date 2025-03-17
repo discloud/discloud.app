d61c2adc588294301633a1e37751aca83ab9fbec
Gorniaky <hwss.s2@gmail.com>
2025-03-17 14:01:01 -0300

    fix user agent

--- packages/rest/src/UserAgent.ts
+++ packages/rest/src/UserAgent.ts
@@ -10,16 +10,19 @@ export default class UserAgent implements IUserAgent {
   constructor(
     readonly prefix?: string,
     readonly version?: string,
-  ) {
-    this.version ??= importJSON<{ version: string }>(PackageLocation).version;
-  }
+  ) { }
 
   #getUserAgent() {
     const osRelease = release().split?.(".").slice(0, 2).join(".") ?? release();
-    return `${this.prefix}/${this.version} (${type()} ${osRelease}; ${platform()}; ${arch()})`;
+    return `${this.prefix}/${this.#getVersion()} (${type()} ${osRelease}; ${platform()}; ${arch()})`;
+  }
+
+  #getVersion() {
+    return this.version ?? (this.#version ??= importJSON<{ version: string }>(PackageLocation).version);
   }
 
   #userAgent!: string;
+  #version!: string;
 
   toString() {
     return this.#userAgent ??= this.#getUserAgent();
