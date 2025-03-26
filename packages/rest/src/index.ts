faf1f8a37d283807c1d74794a822d01e2f0b29cf
Gorniaky <hwss.s2@gmail.com>
2025-03-26 01:24:52 -0300

    util: export UserAgent module in the REST package

--- packages/rest/src/index.ts
+++ packages/rest/src/index.ts
@@ -4,6 +4,7 @@ export * from "./@enum";
 export * from "./@types";
 export * from "./errors";
 export * from "./REST";
+export * from "./UserAgent";
 export * from "./utils";
 
 export const version: string = "[VI]{{inject}}[/VI]";
