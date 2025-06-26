781dae95d4720fee7199d7b951a0e0273e6f79db
Gorniaky <hwss.s2@gmail.com>
2025-06-26 19:57:05 -0300

    refactor: update discloud configuration files and improve validation tests

--- packages/util/src/config/assertions.ts
+++ packages/util/src/config/assertions.ts
@@ -3,47 +3,41 @@ import z from "zod";
 
 const VERSION_REGEXP = /^(current|latest|lts|suja|(?:\d+(?:\.(?:\d+|x)){0,2}))$/;
 
+const STRING_ARRAY_REGEXP = /\s*,\s*/;
+
 const APTPredicate = z.union([
   z.array(z.string()),
-  z.string().trim().refine(arg => arg.split(/\s*,\s*/)),
+  z.string().trim().refine(arg => arg.split(STRING_ARRAY_REGEXP)),
 ]);
-
-const AVATARPredicate = z.union([
-  z.string().trim().url(),
-  z.string().max(0),
-]);
-
-const VERSIONPredicate = z.string().trim().regex(VERSION_REGEXP);
 
 const DiscloudConfigCommonRequiredPredicate = z.object({
   [DiscloudConfigScopes.MAIN]: z.string().min(1),
-  [DiscloudConfigScopes.RAM]: z.coerce.number().min(100),
-  [DiscloudConfigScopes.TYPE]: z.enum(["bot", "site"]),
 });
 
 const DiscloudConfigOptionalPredicate = z.object({
   [DiscloudConfigScopes.APT]: APTPredicate,
   [DiscloudConfigScopes.AUTORESTART]: z.coerce.boolean(),
-  [DiscloudConfigScopes.AVATAR]: AVATARPredicate,
+  [DiscloudConfigScopes.AVATAR]: z.string().trim().url(),
   [DiscloudConfigScopes.HOSTNAME]: z.string(),
+  [DiscloudConfigScopes.ID]: z.string(),
+  [DiscloudConfigScopes.MAIN]: z.string(),
   [DiscloudConfigScopes.NAME]: z.string(),
+  [DiscloudConfigScopes.RAM]: z.coerce.number().min(100),
   [DiscloudConfigScopes.START]: z.string(),
   [DiscloudConfigScopes.STORAGE]: z.string(),
-  [DiscloudConfigScopes.VERSION]: VERSIONPredicate,
+  [DiscloudConfigScopes.TYPE]: z.enum(["bot", "site"]),
+  [DiscloudConfigScopes.VERSION]: z.string().trim().regex(VERSION_REGEXP),
   [DiscloudConfigScopes.VLAN]: z.coerce.boolean(),
 }).partial();
 
 const DiscloudConfigBotPredicate = z.object({
-  [DiscloudConfigScopes.MAIN]: z.string().min(1),
-  [DiscloudConfigScopes.TYPE]: z.string().refine(arg => arg === "bot"),
-  [DiscloudConfigScopes.RAM]: z.coerce.number().min(100),
+  [DiscloudConfigScopes.TYPE]: z.string().refine(arg => !arg || arg === "bot").optional(),
 });
 
 const DiscloudConfigSitePredicate = z.object({
-  [DiscloudConfigScopes.MAIN]: z.string().min(1),
   [DiscloudConfigScopes.TYPE]: z.string().refine(arg => arg === "site"),
   [DiscloudConfigScopes.ID]: z.string().min(1),
-  [DiscloudConfigScopes.RAM]: z.coerce.number().min(512),
+  [DiscloudConfigScopes.RAM]: z.coerce.number().min(512).optional(),
 });
 
 export const DiscloudConfigPredicate =
