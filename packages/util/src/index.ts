e708014a0972c82312312b1f9e94915235f6d61a
Gorniaky <30408913+Gorniaky@users.noreply.github.com>
2026-07-11 16:49:09 -0300

    refactor: update FlexibleBuffer methods and tests for improved clarity and functionality (#1222)

--- packages/util/src/index.ts
+++ packages/util/src/index.ts
@@ -3,11 +3,10 @@ import "source-map-support/register";
 export * from "./BitField";
 export * from "./config";
 export * from "./FileResolver";
+export * from "./FlexibleBuffer";
 export * from "./glob";
 export * from "./JSON";
 export * from "./ModPermissionsBF";
 export * from "./utils";
-
-export { FlexibleBuffer } from "./FlexibleBuffer";
 
 export const version: string = "__PACKAGE_VERSION__";
